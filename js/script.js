83f2da82bce6b53699e36010c2a4e0cf9dfffd49
boluwatifee <bjemiriye@gmail.com>
2020-07-31 11:18:42 +0100

    First Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* const pickBtn = document.querySelectorAll(".btn");
+
+    function pick(){
+        pickBtn.style.background = "#51DF70";
+        pickBtn.style.color = "#00891E"
+    }
+
+    pickBtn.addEventListener("click",pick); */
